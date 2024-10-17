568414e706f9c8ad2958f4cd112cd21ac7bd5d8d
Jordan Sumner <144553157+JordanSum@users.noreply.github.com>
2024-10-17 14:41:48 -0700

    enhace security practice

--- modules/ec2/SG.tf
+++ modules/ec2/SG.tf
@@ -26,7 +26,7 @@ resource "aws_security_group" "instance-SG" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["<your-IP-address/32>"]
   }
 
   egress {
